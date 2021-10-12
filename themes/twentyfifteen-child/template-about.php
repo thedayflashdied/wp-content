6740ae4622a78547bc1083d237d83e1972168153
Matthew Farlymn <matthewfarlymn@gmail.com>
2021-10-12 12:26:47 -0400

    WordPress setup

---
+++ themes/twentyfifteen-child/template-about.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: About
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<h1>This is a custom About page template</h1>
+
+			<?php
+			// Start the loop.
+			while ( have_posts() ) :
+				the_post();
+
+				// Include the page content template.
+				get_template_part( 'content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+				// End the loop.
+			endwhile;
+			?>
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
